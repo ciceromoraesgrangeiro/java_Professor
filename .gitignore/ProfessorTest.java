a82d08f87503fcf86854104049d5834b847db7e4
ciceromoraesgrangeiro <32551312+ciceromoraesgrangeiro@users.noreply.github.com>
2017-10-11 16:50:49 -0300

    Create ProfessorTest.java

---
+++ .gitignore/ProfessorTest.java
@@ -0,0 +1,24 @@
+// package test;
+import professor.Professor;
+
+public class ProfessorTest{
+  public static void main(String[] args){
+  
+    Professor prof = new Professor();
+    prof.cpf ="111.122.131-20";
+    prof.nome="Jurandir";
+    prof.rg="20201-3";
+    prof.matricula="1121";
+    
+    Professor prof2=new Profess();
+    prof2.cpf="222.222.222-22";
+    prof2.nome="Juciclei";
+    prof2.rg="9982-2";
+    prof2.matricula="0921c";
+    
+    prof.imprime(prof);
+    
+    prof.imprime(prof2);
+ 
+   }
+}
