f6173c2173fef9847f47f1b600563cbecb60b86a
ciceromoraesgrangeiro <32551312+ciceromoraesgrangeiro@users.noreply.github.com>
2017-10-11 16:39:38 -0300

    Update professor.java

--- .gitignore/professor.java
+++ .gitignore/professor.java
@@ -1,1 +1,17 @@
+package professor;
 
+public class Professor{
+  public String nome;
+  public String matricula;
+  public String rg;
+  public String cpf;
+  
+  public void imprime(Professor aux){
+    System.out.println("Matricula: "+aux.matricula);
+    System.out.println("CPF: "+aux.cpf);
+    System.out.println("RG: "+aux.rg);
+    System.out.println("Nome: "+aux.nome);
+    System.out.println("----------------------";
+  }
+}
+ 
